a76fd5d7b075c73d0d545f8a3427dd08fe76c41d
Ashwans7 <ashwanshrestha.kun@gmail.com>
2025-06-14 13:25:55 +0545

    Day 2 of learning javascript

---
+++ day2.js
@@ -0,0 +1,24 @@
+
+    
+    // console.log("Hello World!!") // print
+    // console.log(1+1) 
+    // console.log(12*12)
+    // console.log("Ashwan") 
+
+    //variables
+    let cup = "water"
+    var gamala = "saagh" //string
+
+    const pi = 3.14 //numbers
+    const gravity = 9.8
+     
+    let isNepali = true //boolean
+    let isIndian = false
+
+console.log(cup)
+console.log("Mero gamalama ma chai yo cha?" + gamala + " xa")
+console.log(`mero gamala ma chai yo cha? k cha? ${gamala} xa`)
+
+let age = 22
+console.log("My age is " + age)
+console.log(`My age is ${age}`)
